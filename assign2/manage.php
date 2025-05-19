35fae58fba74a8a9d08407adb042ec0379cb8bc5
itsmmarc <officialmarcusmifsud@gmail.com>
2025-05-19 14:34:51 +1000

    create outline of stage 5 HR manager queries

---
+++ assign2/manage.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="author" content="SegFault Services">
+    <meta name="charset" content="utf-8">
+    <meta name="keywords" content="computing, recruiting, services">
+    <title>Manage Enquiries - SegFault Services</title>
+    <!-- Adding in the global styles -->
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="manifest" href="site.webmanifest">
+</head>
+
+<body>
+    <header>
+        <!--add php nav bar-->
+    </header>
+
+    <section class="slideshow center-flex">
+        <h1>Manage Enquiries</h1><br>
+        <div>
+            <p class="newline-flex"><span class="terminalblink">_</span>View and manage all applicants!</p>
+        </div>
+    </section>
+
+    <section class="focus">
+        <h1><span class="terminalblink">_</span>EOIs</h1>
+
+        <!--EOI TABLE
+                Buttons to:
+                    - list all EOIs
+                    - List all EOIs for a position (job reference number, dropdown?)
+                    - List all EOIs for an applicant (name inputs)
+                    - Delete all EOIs for a position (job reference number, dropdown?)
+                    - Change the status of an EOI (dropdown)
+        -->
+        <?php
+
+        ?>
+
+
+    </section>
+
+    <!--replace with php footer-->
+    <section class="pagefooter">
+        <img src="logo.png" alt="Segfault logo" width="300">
+        <aside>
+            <ul>
+                <li><a href="mailto:100589839@student.swin.edu.au">&#9993; Mail Us</a></li>
+            </ul>
+        </aside>
+    </section>
+
+    <footer class="staticfooter center-text">
+        <span>COS10032 Assignment One</span>
+    </footer>
+</body>
+
+</html>
