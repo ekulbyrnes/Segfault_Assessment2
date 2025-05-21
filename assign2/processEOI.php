7225df8decbbc914736e99f6ef886f1a9a26907d
VandyAum <thy.vandy078@gmail.com>
2025-05-21 15:02:19 +1000

    update step 2 and step 3

---
+++ assign2/processEOI.php
@@ -0,0 +1,4 @@
+<?php
+    require_once "settings.php";
+    $conn = mysqli_connect ($host,$user,$pwd,$sql_db);
+?>
--- assign2/settings.php
+++ assign2/settings.php
@@ -1,6 +1,6 @@
 <?php
 	$host = "feenix-mariadb.swin.edu.au";
-	$user = "s105875038"; // your user name
-	$pwd = "100500"; // your password (date of birth ddmmyy unless changed)
-	$sql_db = "s105875038_db"; // your database
+	$user = "s105715697"; // your user name
+	$pwd = "NotCool25"; // your password (date of birth ddmmyy unless changed)
+	$sql_db = "s105715697_db"; // your database
 ?>
