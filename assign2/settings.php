c3df027e02d566026d16c5610d08dbe46f4cf425
itsmmarc <officialmarcusmifsud@gmail.com>
2025-05-21 15:59:18 +1000

    add $conn to settings.php

--- assign2/settings.php
+++ assign2/settings.php
@@ -3,4 +3,6 @@
 	$user = "s105715697"; // your user name
 	$pwd = "NotCool25"; // your password (date of birth ddmmyy unless changed)
 	$sql_db = "s105715697_db"; // your database
+
+    $conn = @mysqli_connect($host,$user,$pwd,$sql_db);
 ?>
